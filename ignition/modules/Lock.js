397becd1386f47e5c72dbb63d8463cb996920e06
mendsalbert <mendsalbert@gmail.com>
2024-07-25 19:24:54 +0000

    auditai 🚀

--- ignition/modules/Lock.js
+++
@@ -1,7 +0,0 @@
-const { buildModule } = require("@nomicfoundation/hardhat-ignition/modules");
-// exporiting smart contract address
-module.exports = buildModule("AuditAI", (m) => {
-  const AuditAI = m.contract("AuditAI");
-
-  return { AuditAI };
-});
